ecf73ed5a2104f8676c761a38b1e07581d30facb
SFNTK <153030492+SFNTK@users.noreply.github.com>
2024-11-17 15:32:43 +0100

    Update nav.jsx

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -5,19 +5,11 @@ import { useRouter } from 'next/navigation'
 import { Menu, X } from 'lucide-react';
 
 
-const Nav = () => {
+const Nav = ({setopen,refer}) => {
     const router = useRouter()
-    const[isopen,setopen]=useState(false)
-    const refer=useRef()
-    useEffect(()=>{
-        if(isopen){
-            refer.current.style.transform="translateX(0px)"
 
-        }
-        else{
-             refer.current.style.transform="translateX(1000px)"
-        }
-    },[isopen])
+  
+  
     return (
         <div id='nav' className="bg-darkprimarycolor text-brightwhite">
            <img src='./logo.png' alt='logo'/>
@@ -44,31 +36,7 @@ const Nav = () => {
             }}>GET A FREE QUOTE</div>
                 <Menu id='burger' onClick={()=>{setopen(true)}}/>
 
-            <div id='navphone' ref={refer} className='bg-darkprimarycolor'>
-                <X id='closebtn' onClick={
-                    ()=>{
-                        setopen(false)
-                    }
-                }/>
-            <ul>
-            <li className='hover:text-warm '   onClick={()=>{
-                router.push('/')
-            }}>Home</li>
-            <li className='hover:text-warm'  onClick={()=>{
-                router.push('/pricing')
-            }}>Pricing</li>
-            <li className='hover:text-warm'   onClick={()=>{
-                router.push('/gallery')
-            }} >Gallery</li>
-            <li className='hover:text-warm'   onClick={()=>{
-                router.push('/testimontials')
-            }}>Testimentials</li>
-         
-            <li className='hover:text-warm'   onClick={()=>{
-                router.push('/policies')
-            }}>Service Policies</li>
-           </ul>
-            </div>
+       
         </div>
     );
 }
